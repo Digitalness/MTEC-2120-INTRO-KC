25f5bf06c2c7298e3cabcdd5abeb7ed3d6dc0cc4
Student <78831490+Arturo101@users.noreply.github.com>
2023-02-10 17:44:29 -0500

    Updated more

---
+++ Assets/MyFirstScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+public class MyFirstScript : MonoBehaviour
+{
+
+    public float speed = 5;
+    public Color color;
+
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        rend = GetComponent<>
+    }
+    public Color GetRandomColor()
+    { 
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
